34df34f9933691071a506d704b85fde23d7d311a
xubury <xubury97@gmail.com>
2020-12-26 19:16:30 +0800

    * better test output

--- test.cpp
+++ test.cpp
@@ -4,28 +4,41 @@
 #include "utils/IO.hpp"
 #include "utils/Validate.hpp"
 
-int main() {
+void TestMergeSort(int num, int size, int val_min, int val_max) {
     std::random_device rd;
     std::mt19937 mt(rd());
 
-    // merge sort test
-    for (int i = 0; i < 10; ++i) {
-        std::uniform_int_distribution<int> dist(0, 256);
-        std::uniform_int_distribution<int> val_dist(
-            std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+    std::cout << "Running merge sort of total " << num
+              << " random tests input\n";
+    std::cout << "contanier size = [0," << size << "], value =[" << val_min
+              << "," << val_max << "]\n";
+    std::vector<std::vector<int>> failed_tests;
+    for (int i = 0; i < num; ++i) {
+        std::uniform_int_distribution<int> dist(1, size);
+        std::uniform_int_distribution<int> val_dist(val_min, val_max);
         std::vector<int> test(dist(mt));
         for (auto &t : test) {
             t = val_dist(mt);
         }
         Sorter::MergeSort(test.begin(), test.end());
-        if (ValidateOrder(test.begin(), test.end())) {
-            std::cout << "Passed!\n";
-        } else {
-            io::PrintVector(test.begin(), test.end());
-            std::cout << "Failed!\n";
+        if (!ValidateOrder(test.begin(), test.end())) {
+            failed_tests.emplace_back(test);
         }
     }
-
+    std::cout << "Test result:\n Passed:" << num - failed_tests.size() << "/"
+              << num << "\n";
+    if (!failed_tests.empty()) {
+        std::cout << "Failing test:\n";
+    }
+    for (auto test : failed_tests) {
+        io::PrintVector(test.begin(), test.end());
+    }
     std::flush(std::cout);
+}
+
+int main() {
+    TestMergeSort(10, 256, std::numeric_limits<int>::min(),
+                  std::numeric_limits<int>::max());
+
     return 0;
 }
