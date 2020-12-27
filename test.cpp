0c3c44821f1629abfec08d5bfc90265f3dd4ead9
xubury <xubury97@gmail.com>
2020-12-27 13:54:18 +0800

    * modify test function

--- test.cpp
+++ test.cpp
@@ -6,7 +6,8 @@
 #include "utils/Timer.hpp"
 #include "utils/Validate.hpp"
 
-void TestMergeSort(int num, int size, int val_min, int val_max) {
+template <typename T, typename T2>
+void TestSorting(int num, int size, T val_min, T val_max, T2 func) {
     std::random_device rd;
     std::mt19937 mt(rd());
 
@@ -15,15 +16,15 @@ void TestMergeSort(int num, int size, int val_min, int val_max) {
     std::cout << "contanier size = [1," << size << "], value =[" << val_min
               << "," << val_max << "]\n";
     WallTimer timer;
-    std::vector<std::vector<int>> failed_tests;
+    std::vector<std::vector<T>> failed_tests;
     for (int i = 0; i < num; ++i) {
-        std::uniform_int_distribution<int> dist(1, size);
-        std::uniform_int_distribution<int> val_dist(val_min, val_max);
+        std::uniform_int_distribution<T> dist(1, size);
+        std::uniform_int_distribution<T> val_dist(val_min, val_max);
         std::vector<int> test(dist(mt));
         for (auto &t : test) {
             t = val_dist(mt);
         }
-        MergeSort::Sort(test.begin(), test.end());
+        func(test.begin(), test.end(), std::less<T>());
         if (!ValidateOrder(test.begin(), test.end())) {
             failed_tests.emplace_back(test);
         }
@@ -35,49 +36,16 @@ void TestMergeSort(int num, int size, int val_min, int val_max) {
     if (!failed_tests.empty()) {
         std::cout << "Failing test:\n";
     }
-    for (auto test : failed_tests) {
-        io::PrintVector(test.begin(), test.end());
-    }
-    std::flush(std::cout);
-}
-
-void TestQuickSort(int num, int size, int val_min, int val_max) {
-    std::random_device rd;
-    std::mt19937 mt(rd());
-
-    std::cout << "Running quick sort test of total " << num
-              << " random tests input\n";
-    std::cout << "contanier size = [1," << size << "], value =[" << val_min
-              << "," << val_max << "]\n";
-    WallTimer timer;
-    std::vector<std::vector<int>> failed_tests;
-    for (int i = 0; i < num; ++i) {
-        std::uniform_int_distribution<int> dist(1, size);
-        std::uniform_int_distribution<int> val_dist(val_min, val_max);
-        std::vector<int> test(dist(mt));
-        for (auto &t : test) {
-            t = val_dist(mt);
-        }
-        QuickSort::Sort(test.begin(), test.end());
-        if (!ValidateOrder(test.begin(), test.end())) {
-            failed_tests.emplace_back(test);
-        }
-    }
-    std::cout << "Test Finished, took " << timer.GetElapsedMS()
-              << " millisecond.\n";
-    std::cout << "Test result:\n Passed:" << num - failed_tests.size() << "/"
-              << num << "\n";
-    if (!failed_tests.empty()) {
-        std::cout << "Failing test:\n";
-    }
-    for (auto test : failed_tests) {
+    for (auto &test : failed_tests) {
         io::PrintVector(test.begin(), test.end());
     }
     std::flush(std::cout);
 }
 
 int main() {
-    TestMergeSort(100, 20, -324, 700);
-    TestQuickSort(199, 142, -1202, 3020);
+    TestSorting(100, 20, -324, 700,
+                &MergeSort::Sort<std::vector<int>::iterator>);
+    TestSorting(100, 20, -324, 700,
+                &QuickSort::Sort<std::vector<int>::iterator>);
     return 0;
 }
